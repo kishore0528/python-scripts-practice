59232f936b201f81f97b0a60290915ec085eb6a3
Kishore <kishorekumaar28@gmail.com>
2026-02-25 10:48:51 +0530

    Task 1 and fetching abnner while checking

--- 4_task_1_port_scanning_with_banner.py
+++ 4_task_1_port_scanning_with_banner.py
@@ -1,29 +1,29 @@
 import socket
 import threading as th
 
-def port_checker(ip, port):
+def port_scan(ip, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(1)
     result = s.connect_ex((ip, port))
 
     if not result:
-        print(f"Port {port} is open!")
         try:
             banner = s.recv(1024)
-            print(f"Banner: {banner.decode().strip()}")
+            print(f"Port {port} is OPEN. Banner: {banner.decode().strip()}")
         except:
-            print("No banner returned.")
+            print(f"Port {port} is OPEN but silent.")
 
     s.close()
 
-threads= []
+threads = []
+ports = [21, 22, 53, 80, 443, 223]
 
-for port in range(1,1024):
-    t = th.Thread(target=port_checker, args=("8.8.8.8", port))
+for port in ports:
+    t = th.Thread(target=port_scan, args=("100.125.147.99", port))
     threads.append(t)
     t.start()
 
 for t in threads:
     t.join()
 
-print("Scan Complete!")
+print("Scan complete!")
